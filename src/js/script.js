07c156ea89d26207ef871fc9e7779666edb35ea5
Hello World <joaop.codes@gmail.com>
2024-07-25 22:25:58 -0300

    .

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+function btnAvancar() {
+    alert("acionou");
+    var photo = document.getElementById("foto")
+    photo.style.backgroundImage = "url(../img/fundo.jpg)";
+}
+
+
+
+function btn_voltar() {
+
+}
